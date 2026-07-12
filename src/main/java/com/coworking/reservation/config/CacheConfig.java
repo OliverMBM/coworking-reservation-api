2cc134f4500e27b07596d0c98cf9c9d9423e55bd
Oliver Barahona <oliverbarahona@Mac-mini-de-Oliver.local>
2026-07-11 18:16:49 -0600

    Creando la configuracion del proyecto

---
+++ src/main/java/com/coworking/reservation/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.coworking.reservation.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
